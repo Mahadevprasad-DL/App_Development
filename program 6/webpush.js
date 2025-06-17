a05e3dd57c0d2680cd0f8c9d54b6b478d7f6b93a
Mahadevprasad D L <mahadevprasaddl@gmail.com>
2025-06-17 11:49:00 +0530

    Add files via upload
    Web Notification

---
+++ program 6/webpush.js
@@ -0,0 +1,24 @@
+const webpush = require('web-push');
+
+// VAPID keys should only be generated only once.
+const vapidKeys = {
+  publicKey: 'public key',
+  privateKey: 'private key'
+};
+
+webpush.setVapidDetails(
+  'mailto:mahadevapdl.mca24@rvce.edu.in',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+// This is the same output of calling JSON.stringify on a PushSubscription
+const pushSubscription = {
+  endpoint: '.....',
+  keys: {
+    auth: '.....',
+    p256dh: '.....'
+  }
+};
+
+webpush.sendNotification(pushSubscription, 'This is a web notification');

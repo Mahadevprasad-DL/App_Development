2da397f5572dc3f3192ae836357e7123df8e3308
Mahadevprasad D L <mahadevprasaddl@gmail.com>
2025-06-18 22:42:24 +0530

    Add files via upload

---
+++ program 5/sw.js
@@ -0,0 +1,33 @@
+(function(){
+  self.addEventListener('install', event => {
+    console.log('service worker installing');
+    event.waitUntil(
+      caches
+        .open('PWD_app')
+        .then(cache => cache.addAll(['/git']))
+    );
+    self.skipWaiting();
+  });
+
+  self.addEventListener('activate', event => {
+    event.waitUntil(caches.delete('PWD_app'));
+    console.log('service worker activating...');
+  });
+
+  self.addEventListener('fetch', event => {
+    console.log('fetching', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(async (response) => {
+        if (response) {
+          return response;
+        }
+        let data = fetch(event.request);
+        let data_clone = (await data).clone();
+        event.waitUntil(
+          caches.open('PWD_app').then(cache => cache.put(event.request, data_clone))
+        );
+        return data;
+      })
+    );
+  });
+})();
